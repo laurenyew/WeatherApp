fc8ab7595459fb366b0823e7849378515d59445b
laurenyew <laurenyew@gmail.com>
2016-04-23 14:38:24 -0500

    Fixing phunware splash screen

--- app/src/main/java/laurenyew/weatherapp/SplashActivity.java
+++ app/src/main/java/laurenyew/weatherapp/SplashActivity.java
@@ -1,10 +1,7 @@
 package laurenyew.weatherapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import laurenyew.weatherapp.list.WeatherListActivity;
 
 /**
  * Created by laurenyew on 4/3/16.
@@ -16,9 +13,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, WeatherListActivity.class);
-        startActivity(intent);
-        finish();
+//
+//        Intent intent = new Intent(this, WeatherListActivity.class);
+//        startActivity(intent);
+//        finish();
     }
 }
